80d8085270f0d3ef22e67552a87115650d24c698
definitelyzoey <definitelyzo4y@gmail.com>
2025-02-21 00:26:48 +1300

    changed/added details and added 'twodo' as an script activation method

--- setup.py
+++ setup.py
@@ -3,27 +3,25 @@ import os
 
 from setuptools import setup, find_packages
 
-
 def read(*parts):
     return codecs.open(os.path.join(os.path.abspath(os.path.dirname(__file__)), *parts), 'r').read()
 
-
 long_description = read('README.md')
 
-
 setup(
-    name='todo',
+    name='twodo',
     version='0.0.1',
-    description='Command-line tool to manage Todo lists',
+    description='CLI tool to manage twodo (todo) lists!',
     long_description=long_description,
-    keywords='todo list task productivity project management',
-    author='Francois Chalifour',
-    author_email='francois.chalifour@gmail.com',
-    url='https://github.com/francoischalifour/todo-cli',
+    keywords='todo list task productivity project-management',
+    author='Francois Chalifour, definitelyzoey',
+    author_email='francois.chalifour@gmail.com, definitelyzo4y@gmail.com',
+    url='https://github.com/francoischalifour/todo-cli and https://github.com/definitelyzoey/twodo',
     license='MIT',
     packages=find_packages(),
     entry_points={
         'console_scripts': [
+            'twodo=todo:main',
             'todo=todo:main'
         ]
     }
